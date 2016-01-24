fe02ec6b2d91bb0c491cc7b814253949f91ec457
Rhett Lowe <rng2ml@gmail.com>
2016-01-24 04:38:11 -0800

    saving

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 /**
  * Created by rhett on 1/23/16.
  */
+'use strict';
 
+var Message = require('./lib/message');
+var fs      = require('fs');
+
+var txt = fs.readFileSync('./test/message.txt').toString();
+
+var a = Message(fs.readFileSync('./test/message.txt'));
+var b = Message(fs.createReadStream('./test/message.txt'));
+var c = Message(txt);
+
+//console.log(a.promise)
+a.then(function(){
+  console.log('a', a);
+});
+b.then(function(){
+  console.log('b', b);
+});
+c.then(function(){
+  console.log('c', c);
+});
