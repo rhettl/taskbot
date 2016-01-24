ae45860f05399fdbfad85678207b01ad53291527
Rhett Lowe <rng2ml@gmail.com>
2016-01-24 15:21:46 -0800

    registry added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var Message = require('./lib/message');
+var Registry = require('./lib/command-registry')();
 var fs      = require('fs');
 
 var txt = fs.readFileSync('./test/message.txt').toString();
@@ -12,13 +13,16 @@ var a = Message(fs.readFileSync('./test/message.txt'));
 var b = Message(fs.createReadStream('./test/message.txt'));
 var c = Message(txt);
 
-//console.log(a.promise)
-a.then(function(){
-  console.log('a', a);
+Registry.register('log', function(args, m){
+  console.log(m);
+  return m;
 });
-b.then(function(){
-  console.log('b', b);
+
+a.commands.forEach(function (cmd) {
+  a = Registry.call(cmd, a);
 });
-c.then(function(){
-  console.log('c', c);
-});
+
+//.done(function(){
+//  console.log(a);
+//})
+;
