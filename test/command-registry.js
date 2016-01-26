6e5dd9b05f3fa768786415e21d74a9584d8fcc13
Rhett Lowe <rng2ml@gmail.com>
2016-01-25 22:35:35 -0800

    command-registry testing

--- test/command-registry.js
+++ test/command-registry.js
@@ -6,9 +6,10 @@
 var assert = require('assert');
 //var path   = require('path');
 //var fs     = require('fs');
-//var q      = require('q');
+var q = require('q');
 
 var Registry = require('../lib/command-registry');
+var Command  = require('../lib/command');
 //var Message      = require('../lib/message');
 //var testFile     = './message.txt';
 //var testFilePath = path.normalize(path.join(__dirname, testFile));
@@ -28,7 +29,7 @@ describe('CommandRegistry', function () {
 
   describe('#register', function () {
     var reg;
-    beforeEach(function(){
+    beforeEach(function () {
       reg = Registry();
     });
 
@@ -88,7 +89,7 @@ describe('CommandRegistry', function () {
 
   describe('#has', function () {
     var reg;
-    beforeEach(function(){
+    beforeEach(function () {
       reg = Registry();
       reg.register('named', function noop () {
         return 1;
@@ -101,15 +102,202 @@ describe('CommandRegistry', function () {
     });
   });
 
+  describe('#_callArray', function () {
+    var reg;
+    var counter;
+    beforeEach(function () {
+      reg     = Registry();
+      counter = 0;
+    });
+
+    it('Calls Registry.call when one item in Command Array', function () {
+      reg.call = function () {
+        counter++;
+      };
+      reg._callArray([1]);
+      assert(counter, 'Registry.call not called');
+    });
+
+    it('recursively calls self when more than one command in array', function (done) {
+      /*
+       This test badly needs reworking.
+       I think I need spies, but I am not really sure
+       */
+
+      var mockup = {
+        call      : function () {
+          // increment counter by one when call() is called
+          counter += 1;
+          return q(1);
+        },
+        _callArray: function (arr) {
+          // increment by ten when _callArray()
+          counter += 10;
+
+          // continue to call _callArray()
+          // that WOULD have been called if not for mockup
+          return reg._callArray.call(mockup, arr.slice(1));
+        }
+      };
+      reg._callArray.call(mockup, [1, 2]).done(function () {
+        assert.equal(12, counter);
+        done();
+      });
+    });
+
+    it('passes done() to final call() function call', function (cb) {
+      /*
+       This test badly needs reworking.
+       I think I need spies, but I am not really sure
+       */
+      var d = function done () {
+        assert(1);
+        cb();
+      };
+
+      var mockup = {
+        call: function (_, __, done) {
+          done();
+        }
+      };
+      reg._callArray.call(mockup, [1], '', d);
+    });
+
+    it('passes done() recursively to _callArray()', function (cb) {
+      /*
+       This test badly needs reworking.
+       I think I need spies, but I am not really sure
+       */
+      var d = function done () {
+        return 1;
+      };
+
+      var mockup = {
+        call      : function () {
+          return q(1);
+        },
+        _callArray: function (_, __, done) {
+          done();
+          cb();
+        }
+      };
+      reg._callArray.call(mockup, [1, 2], '', d);
+    });
+  });
+
+  /*
+   Still working on this one. This is the hardest function to test. :(
+   */
   describe('#call', function () {
-    it('t', function () {
-      assert(true);
+    var reg;
+    beforeEach(function () {
+      reg = Registry();
+    });
+
+    it('passes to _callArray when first argument is an Array', function (done) {
+      var mockup = {
+        _callArray: function () {
+          assert(1);
+          done();
+        }
+      };
+      reg.call.call(mockup, [1, 2]);
+    });
+
+    it('returns a promise if no done() provided', function () {
+      assert(q.isPromise(
+        reg.call('name')
+      ));
+    });
+
+    it('returns a undefined if done() provided', function () {
+      assert.strictEqual(
+        'undefined',
+        typeof reg.call('name', '', function () {
+        })
+      );
+    });
+
+    it('returns a undefined if done() provided', function () {
+      assert.strictEqual(
+        'undefined',
+        typeof reg.call('name', '', function () {
+        })
+      );
+    });
+
+    it('calls a registered function', function (done) {
+      reg.register('done', function () {
+        assert(1);
+        done();
+      });
+      reg.call('done');
+    });
+
+    it('calls registered functions sequentially', function (done) {
+      var string = '';
+      reg.register('done', function () {
+        string += 'first'; // first should be called first
+      });
+      reg.register('done', function () {
+        string += 'second'; // second should be called second
+      });
+
+      assert.equal(2, reg.command('done').length);
+
+      reg.call('done', '', function () {
+        assert.equal('firstsecond', string);
+        done();
+      });
+    });
+
+    it('waits for first to finish before second starts', function (done) {
+      var string    = '';
+      var startTime = Date.now();
+      var delayTime = 200; // ms
+      reg.register('done', function (_, __, cb) {
+        string += 'one'; // run one
+
+        // start delay
+        setTimeout(function () {
+          // run two
+          string += 'two';
+          cb(); // callback for Register function
+        }, delayTime);
+      });
+      reg.register('done', function () {
+        // run three
+        string += 'three';
+      });
+
+      assert.equal(2, reg.command('done').length);
+
+      reg.call('done', '', function () {
+        // check run order
+        assert.equal('onetwothree', string);
+        // check for pause
+        assert(Date.now() - startTime >= delayTime);
+        // callback for it()
+        done();
+      });
+    });
+
+    it('converts string commands to Command before calling', function (done) {
+      reg.register('done', function (commandObject) {
+        assert.strictEqual('string', typeof commandString);
+        assert.strictEqual('object', typeof commandObject);
+        assert(commandObject instanceof Command);
+        done();
+      });
+
+      var commandString = 'done';
+      reg.call(commandString);
     });
   });
 
   describe('#command', function () {
     var reg;
-    beforeEach(function(){
+    beforeEach(function () {
       reg = Registry();
       reg.register('named', function noop () {
         return 1;
