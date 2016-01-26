f886c5781ddff9f5c1603370e69accf1b9699c76
Rhett Lowe <rng2ml@gmail.com>
2016-01-25 22:56:27 -0800

    v0.1.0 and make index

--- index.js
+++ index.js
@@ -6,61 +6,9 @@
 var Command = require('./lib/command');
 var Message = require('./lib/message');
 var Registry = require('./lib/command-registry')();
-var fs      = require('fs');
-var q       = require('q');
 
-var txt = fs.readFileSync('./test/message.txt').toString();
-
-var a = Message(fs.readFileSync('./test/message.txt'));
-//var b = Message(fs.createReadStream('./test/message.txt'));
-//var c = Message(txt);
-
-Registry.register('log', function(cmd, m){
-  console.log(m);
-  return m;
-});
-Registry.register('log', function(cmd, m){
-  console.log(m.commands);
-  return m;
-});
-Registry.register('duplicate', function(cmd, m){
-  var tmp = m.body;
-
-  for (var i = +cmd.args[0] || 1; i > 0; i --) {
-    m.body += tmp;
-  }
-
-  return m;
-});
-Registry.register('pause', function(cmd, m, done){
-  setTimeout(function(){
-    done(null, m);
-  }, +cmd.args[0] || 0);
-});
-
-
-Registry.call(a.commands, a, function(){
-  console.log('Done.');
-});
-
-///*
-//Works as expected
-// */
-//Registry.call(Command('#pause 2000'), a, function(err, data){
-//  console.log('done.1');
-//});
-//Registry.call(Command('#duplicate'), a, function(err, data){
-//  console.log('done.2');
-//});
-
-
-//Registry.call(Command('#pause 2000'), a).then(function () {
-//  return Registry.call(Command('#duplicate'), a, function(err, data){
-//    console.log('done.2');
-//  });
-//});
-
-
-//b.promise.done(function(){
-//  console.log(b);
-//})
+module.exports = {
+  Registry: Registry,
+  Message: Message,
+  Command: Command
+};
